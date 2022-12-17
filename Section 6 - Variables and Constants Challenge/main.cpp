f6d36cc27ad0d0c99055f9ccf23462257989d93b
Dylan Unas <dylanunas@gmail.com>
2022-12-16 22:05:56 -0500

    Completed section 6

---
+++ Section 6 - Variables and Constants Challenge/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    cout << "Hello, welcome to Frank's Carpet Cleaning Service\n\n";
+
+    const double smRoomCost {25};
+    const double lRoomCost {35};
+    const double tax {0.06};
+    double cost {0};
+    int amtSmRooms {0}, amtLRooms{0};
+
+    cout << "How many small rooms would you like to be cleaned? ";
+    cin >> amtSmRooms;
+    cout << "How many large rooms would you like to be cleaned? ";
+    cin >> amtLRooms;
+
+    cost = (amtSmRooms * smRoomCost) + (amtLRooms * lRoomCost);
+
+    cout << "\nEstimate for carpet cleaning service\n"
+    << "Number of small rooms: " << amtSmRooms << "\n"
+    << "Number of large rooms: " << amtLRooms << "\n"
+    << "Price per small room: $" << smRoomCost << "\n"
+    << "Price per large room: $" << lRoomCost << "\n"
+    << "Cost: $" << cost << "\n"
+    << "Tax: $" <<  cost * tax << "\n"
+    << "========================================\n"
+    << "Total estimate: $" << cost + (cost * tax) << "\n"
+    << "This estimate is valid for 30 days\n\n" << endl;
+
+    return 0;
+}
